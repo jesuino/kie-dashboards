4b6bc87c5ee244395f4e9c298a0b89110c005292
William Siqueira <william.fatecsjc@gmail.com>
2022-03-04 02:27:58 -0300

    Adding Uniform Component

---
+++ packages/uniforms-base/src/Uniforms.tsx
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2022 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { JSONSchemaBridge } from "uniforms-bridge-json-schema";
+import { AutoForm } from "uniforms-patternfly";
+import * as Ajv from "ajv";
+import * as React from "react";
+
+const ajv = new Ajv({ allErrors: true, useDefaults: true });
+const createValidator = (schema: any) => {
+  const validator = ajv.compile(schema);
+  return (model: any) => {
+    validator(model);
+    return validator.errors?.length ? { details: validator.errors } : null;
+  };
+};
+
+interface Props {
+  schema: Object;
+}
+
+export const Uniforms = (props: Props) => {
+  const bridge = new JSONSchemaBridge(props.schema, createValidator(props.schema));
+  return (
+    <>
+      <AutoForm schema={bridge} onSubmit={console.log} />
+    </>
+  );
+};
